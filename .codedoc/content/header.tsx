9f75ae8c441751cbe423278a04b53c8b75181587
Shai Mendel <shaimendel@gmail.com>
2020-05-03 01:18:39 +0300

    feat: remove watermark

--- .codedoc/content/header.tsx
+++ .codedoc/content/header.tsx
@@ -4,18 +4,7 @@ import { Header as _Header, GithubButton, Watermark } from '@codedoc/core/compon
 
 export function Header(config: CodedocConfig, renderer: any) {
   return (
-    <_Header>{config.misc?.github ?
-      <fragment>
-        <GithubButton action={config.misc.github.action || 'Star'}
-          repo={config.misc.github.repo}
-          user={config.misc.github.user}
-          large={config.misc.github.large === true}
-          count={config.misc.github.count !== false}
-          standardIcon={config.misc.github.standardIcon !== false}/>
-        <br/><br/>
-      </fragment>
-      : ''}
-      <Watermark/>
+    <_Header>
     </_Header>
   )
 }
